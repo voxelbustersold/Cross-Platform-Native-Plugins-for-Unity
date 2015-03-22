1ed558b130461284bfebcb55676588959a75b82b
Ayyappa <ayyappa.99@gmail.com>
2015-03-23 01:57:16 +0530

    *Grouped doxygen output

---
+++ Documentation/DoxygenOutput/html/navtreedata.js
@@ -0,0 +1,28 @@
+var NAVTREE =
+[
+  [ "Native Plugins Api", "index.html", [
+    [ "Package Functions", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ],
+      [ "Enumerator", "namespacemembers_eval.html", null ]
+    ] ],
+    [ "Classes", null, [
+      [ "Class List", "annotated.html", "annotated" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Properties", "functions_prop.html", null ],
+        [ "Events", "functions_evnt.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
